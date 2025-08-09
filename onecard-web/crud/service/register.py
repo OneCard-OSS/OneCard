91c44b9402a65d0e5099c6e2cc531286013ef64d
tlsnwls <tlsdnwls0103@gmail.com>
2025-08-09 22:07:58 +0900

    Fix: Changed redirect uri function functionality

--- onecard-web/crud/service/register.py
+++ onecard-web/crud/service/register.py
@@ -1,26 +1,31 @@
 from fastapi import HTTPException, Depends
 from sqlalchemy.orm import Session
-from models.user import Users
+from uuid import UUID
 from models.service import Services, RedirectUris
 from schemas.service import RegisterService, AddRedirectUris
-from utils.current_user import current_user_info
 from utils.gen import gen_client_secret, gen_client_id
 
 def create_service(service:RegisterService, 
                      db:Session,
                      current_user:dict):
-    id = current_user["id"]
-    print(f"User ID:{id}")
-    if not id:
-        raise HTTPException(status_code=400,
-                            detail="Invalid User")
+    """
+    If you are a logged in administrator, you can register.
+    Create a new service.
+    Args:
+    - service: Register DTO containing service registration details(name, description)
+    - db: ORM Session
+    - current_user: JWT-based current user information
+    Returns:
+    - Services: The newly created service ORM object
+    """
+    
     client_id = gen_client_id()
     client_secret = gen_client_secret()
         
     new_service = Services(
         client_id = client_id,
         client_secret = client_secret,
-        owner_id = id,
+        owner_id = current_user["id"],
         name = service.name,
         description = service.description
     )
@@ -30,20 +35,36 @@ def create_service(service:RegisterService,
     
     return new_service 
         
-def add_redirect_uris(client_id:str,
-                      uris:AddRedirectUris, 
-                      db:Session,
-                      current_user:dict):
-    id = current_user["id"]
-    service = db.query(Services).filter(Services.client_id == client_id,
-                                        Services.owner_id == id).first()
+def add_or_update_redirect_uris(client_id:str,
+                                uris:AddRedirectUris, 
+                                db:Session,
+                                current_user:dict):
+    """
+    Add or Update redirect URIs for a given service.
+    This function allows only the administrator 
+    who registered the service to manipulate the redirect URIs.
+    
+    Args:
+    - client_id: The client ID of the service
+    - uris: DTO containing the list of redirect URIs to register
+    - db: ORM Session
+    - current_user: JWT-based current user information
+    Returns:
+    - dict: Confirmation message indicating success
+    """
+    
+    service = db.query(Services).filter(Services.client_id == client_id).first()
     if not service:
         raise HTTPException(status_code=404,
-                            detail="Service not found or don't have permission to modify it")
+                            detail="Service not found")
+    if  UUID(current_user["id"]) != service.owner_id:
+        raise HTTPException(status_code=403,
+                            detail="Permission denied")
+    
+    db.query(RedirectUris).filter(RedirectUris.client_id == client_id).delete()
     for uri in uris.redirect_uris:
         redirect_uri = RedirectUris(client_id=service.client_id,
-                                    uris=str(uri)
-        )
+                                    uris=str(uri))
         db.add(redirect_uri)
     
     db.commit()
