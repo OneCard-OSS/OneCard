092eb9da40e3b05d19a48c2375a5a7c753de6981
tlsnwls <tlsdnwls0103@gmail.com>
2025-08-09 22:06:36 +0900

    Feat: Add Service Lookup DTO

--- onecard-web/schemas/service.py
+++ onecard-web/schemas/service.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, HttpUrl
-from typing import List
+from typing import List, Optional
+from datetime import datetime
 
 class RegisterService(BaseModel):
     name : str
@@ -7,4 +8,15 @@ class RegisterService(BaseModel):
 
 class AddRedirectUris(BaseModel):
     redirect_uris : List[HttpUrl]
+
+class GetSerivceInfo(BaseModel):
+    client_id : str
+    name : str
+    description : Optional[str]
+    client_secret : str
+    created_at : datetime
+    owner : str
+    redirect_uris : List[HttpUrl]
     
+    class Config:
+        from_attributes : True
