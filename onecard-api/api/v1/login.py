278c2bcd24393da3ae297515fa14512f5786566a
tlsnwls <tlsdnwls0103@gmail.com>
2025-08-12 22:19:22 +0900

    Fix: added type annotation

--- onecard-api/api/v1/login.py
+++ onecard-api/api/v1/login.py
@@ -1,12 +1,13 @@
 from fastapi import APIRouter, Query, Depends
 from sqlalchemy.orm import Session
 from core.database import get_db
+from schemas.login import InitLoginRequest
 from services.init_login import init_login
 
 login_router = APIRouter(prefix="/api/v1")
 
 @login_router.post("/login")
-def login(emp_no:str,
+def login(emp_no:InitLoginRequest,
           client_id:str=Query(...),
           redirect_uri:str=Query(...),
           db:Session=Depends(get_db)):
