1ba6cdfbfd379e442a878aeda201cff9a6c01293
tlsnwls <tlsdnwls0103@gmail.com>
2025-08-13 20:52:37 +0900

    delted

--- onecard-api/schemas/login.py
+++
@@ -1,4 +0,0 @@
-from pydantic import BaseModel
-
-class InitLoginRequest(BaseModel):
-    emp_no:str
--- onecard-api/schemas/oauth.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-class RequestToken(BaseModel):
-    grant_type:str
-    client_secret:str
-    redirect_uri:str
-    code:str
