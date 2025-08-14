aeb476374f61ca9978c5d903926f0d9e279b1a83
tlsnwls <tlsdnwls0103@gmail.com>
2025-08-14 17:32:34 +0900

    Feat: added logger setup

--- onecard-api/main.py
+++ onecard-api/main.py
@@ -3,7 +3,11 @@ import uvicorn
 from api.v1.login import login_router
 from api.v1.oauth import oauth_router
 from api.v1.nfc_polling import nfc_router
+from api.v1.card_response import card_response_router
 from api.v1.manage_card import card_router
+from logging_config import setup_logging
+
+setup_logging()
 
 app = FastAPI()
 
@@ -12,6 +16,7 @@ def main():
     return{"message" : "Hello from onecard-api!"}
 
 app.include_router(login_router)
+app.include_router(card_response_router)
 app.include_router(nfc_router)
 app.include_router(oauth_router)
 app.include_router(card_router)
