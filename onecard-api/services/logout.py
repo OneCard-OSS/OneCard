5d24f50004b0b0d6d2163646c3ee053ce66c6070
tlsnwls <tlsdnwls0103@gmail.com>
2025-08-14 17:37:10 +0900

    Feat: added logout

---
+++ onecard-api/services/logout.py
@@ -0,0 +1,44 @@
+from logging import LoggerAdapter
+from core.redis import redis_config
+from utils.redis_const import REDIS_REFRESH_TOKEN_PREFIX, REDIS_SESSION_PUB_MAP_PREFIX, REDIS_PUB_SESSION_MAP_PREFIX
+
+rd = redis_config()
+
+def logout_user(s_id:str,
+                access_token:str,
+                logger:LoggerAdapter):
+    """
+    Logout Service
+    Args:
+    - access_token
+    - logger
+    Returns:
+    - dict
+    """
+    logger.debug(f"Received token:{access_token}")
+    # STEP 1. Verifying Token and Get session id
+        
+    # STEP 2. Delete refresh token and session in Redis
+    refresh_token_key =  f"{REDIS_REFRESH_TOKEN_PREFIX}{s_id}"
+    session_pub_key = f"{REDIS_SESSION_PUB_MAP_PREFIX}{s_id}"
+
+    rd.delete(refresh_token_key)
+    
+    user_pubkey = rd.get(session_pub_key)
+    if user_pubkey:
+        pubkey_session_key = f"{REDIS_PUB_SESSION_MAP_PREFIX}{user_pubkey.decode('utf-8')}"
+        rd.delete(pubkey_session_key)
+        rd.delete(session_pub_key)
+        logger.info("Session and RefreshToken successfully invalidated", extra={"s_id":s_id})
+    else:
+        logger.warning("Session key not found for s_id. Already logged out?", extra={"s_id":s_id})
+    
+    log_extra = {
+        "s_id" : s_id,
+        "action" : "logout",
+        "status" : "success",
+        "message" : "User logged out successfully"
+    }
+    logger.info("Logout process complete", extra=log_extra)
+
+    return {"message" : "Logout Successful"}
