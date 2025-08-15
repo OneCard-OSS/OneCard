ef574c8a21d50e91ecf5a7e89ca3e73453f658dd
tlsnwls <tlsdnwls0103@gmail.com>
2025-08-15 22:23:53 +0900

    Feat: to establish manytomany relationship

---
+++ onecard-web/models/employee.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Column, String, Date
+from sqlalchemy.orm import relationship
+from core.database import Base
+from models.permission import permission_table
+
+class Employee(Base):
+    __tablename__ = "employee"
+    
+    emp_no = Column(String(16), primary_key=True, unique=True, nullable=False)
+    name = Column(String, nullable=False)
+    phone_num = Column(String, nullable=False)
+    position = Column(String, nullable=False)
+    department = Column(String, nullable=False)
+    birth = Column(Date, nullable=False)
+    email = Column(String, nullable=False)
+    
+    services = relationship(
+        "Services",
+        secondary=permission_table,
+        back_populates="employees"
+    )
