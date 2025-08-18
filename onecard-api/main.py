c93bbf6e75f3e0182a7f3c83e9f9e242a01d6a58
tlsnwls <tlsdnwls0103@gmail.com>
2025-08-19 01:10:34 +0900

    Chore: delete unused router

--- onecard-api/main.py
+++ onecard-api/main.py
@@ -4,7 +4,6 @@ from api.v1.login import login_router
 from api.v1.oauth import oauth_router
 from api.v1.nfc_polling import nfc_router
 from api.v1.card_response import card_response_router
-from api.v1.manage_card import card_router
 from logging_config import setup_logging
 
 setup_logging()
@@ -19,7 +18,6 @@ app.include_router(login_router)
 app.include_router(card_response_router)
 app.include_router(nfc_router)
 app.include_router(oauth_router)
-app.include_router(card_router)
 
 if __name__ == "__main__":
     uvicorn.run(app, 
